eaa681f8768b848354951ad9423ae9ba1c1c763f
rahul8848 <rahul8848@gmail.com>
2021-08-20 13:30:26 -0700

    Phase 1 - Returning the defintion of a given word.

--- Proj1.py
+++ Proj1.py
@@ -1,8 +1,17 @@
 
 import json
 data = json.load(open("./data.json")) # default mode is read mode
-print(type(data)) # data type of data 
-print(data) # printing contents of the data
+
+# method that returns the definition of a word
+def translate(word):
+	return data[word]
+
+word = input("Enter a word: ")
+print(translate(word))
+
+
+
+
 
 
 
