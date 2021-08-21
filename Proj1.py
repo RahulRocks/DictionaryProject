03c8623b844462563b8c536208e97da18f256173
rahul8848 <rahul8848@gmail.com>
2021-08-20 18:02:59 -0700

    Phase 2 - Handling words that do not exist

--- Proj1.py
+++ Proj1.py
@@ -3,11 +3,14 @@ import json
 data = json.load(open("./data.json")) # default mode is read mode
 
 # method that returns the definition of a word
-def translate(word):
-	return data[word]
+def lookup(word):
+	if word in data:
+		return data[word]
+	else:
+		return "The word doesn't exist. Please double check it."
 
-word = input("Enter a word: ")
-print(translate(word))
+userword = input("Enter a word: ")
+print(lookup(userword))
 
 
 
