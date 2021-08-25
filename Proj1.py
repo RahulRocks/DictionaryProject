1a5bffb8d652add0ed356d8dc639ef1c9fdae377
rahul8848 <rahul8848@gmail.com>
2021-08-24 17:01:46 -0700

    Phase 6 - Optimizing final output

--- Proj1.py
+++ Proj1.py
@@ -10,18 +10,28 @@ def lookup(word):
 		return data[word]
 	# Finding the closest match of the word
 	elif len(get_close_matches(word,data.keys())) > 0:
+		# Confirming user input
 		yn =  input("Did you mean %s instead? Enter Y if yes and N if no. " % get_close_matches(word,data.keys())[0])
+		
 		if yn == "Y":
 			return data[get_close_matches(word,data.keys())[0]]
 		elif yn == "N":
 			return "The word doesn't exist.Please double check it."
 		else:
 			return "We didn't understand your entry."
+	
 	else:
 		return "The word doesn't exist. Please double check it."
 
 userword = input("Enter a word: ")
-print(lookup(userword))
+#print(lookup(userword))
+output = lookup(userword)
+
+if type(output) == list:
+	for item in output:
+		print(item)
+else:
+	print(output)
 
 
 
